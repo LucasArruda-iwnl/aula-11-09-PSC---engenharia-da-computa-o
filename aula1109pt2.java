6659bbed46475d72c35ea927d69cebfbd19617f6
Lucas Arruda <105546865+LucasArruda-iwnl@users.noreply.github.com>
2023-09-11 21:37:50 -0300

    Add files via upload

---
+++ aula1109pt2.java
@@ -0,0 +1,8 @@
+public class aula1109pt2 {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
---
+++ exercicio1109.java
@@ -0,0 +1,12 @@
+public class exercicio1109 {
+    public static void main(String[] args) {
+        int i; 
+
+        System.out.println("Neste programa iremos exibir os 100 primeiros números naturais segue:");
+
+        for (i=0; i<=99; i++) { 
+            System.out.println("Os números naturais são" + i);
+        }
+    }
+    
+}
